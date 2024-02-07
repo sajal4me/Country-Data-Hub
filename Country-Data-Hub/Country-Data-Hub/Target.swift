7cccb33dc7685e9a7b061e26d566cd03cb9902c9
Sajal Gupta <sajalgupta4me@gmail.com>
2024-02-07 18:55:00 +0530

    implement `Endpoint  protocol to Target enum` and provide URL path and values

--- Country-Data-Hub/Country-Data-Hub/Target.swift
+++ Country-Data-Hub/Country-Data-Hub/Target.swift
@@ -10,3 +10,41 @@ import Foundation
 internal enum Target {
     case countries
 }
+
+extension Target: Endpoint {
+    enum Method: String {
+        case get = "GET"
+    }
+    
+    var scheme: String {
+        return "https"
+    }
+    
+    var baseURL: String{
+        return "api.sampleapis.com"
+    }
+    
+    var path: String {
+        switch self {
+        case .countries:
+            return "/countries/countries"
+        }
+    }
+    
+    var parameters: [URLQueryItem] {
+        let param = [String: String]()
+        switch self {
+        case .countries:
+            return param.map {
+                URLQueryItem(name: $0.0, value: $0.1)
+            }
+        }
+    }
+    
+    var method: String {
+        switch self {
+        case .countries:
+            return Method.get.rawValue
+        }
+    }
+}
