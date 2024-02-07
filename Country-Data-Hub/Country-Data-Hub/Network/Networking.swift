4ee5fc1dd18ebb4299ec3124ac797a39f371d56a
Sajal Gupta <sajalgupta4me@gmail.com>
2024-02-07 19:05:33 +0530

    define `Networking class` that implement `NetworkProtocol` with data loading and parsing operation

--- Country-Data-Hub/Country-Data-Hub/Network/Networking.swift
+++ Country-Data-Hub/Country-Data-Hub/Network/Networking.swift
@@ -10,3 +10,42 @@ import Foundation
 protocol NetworkProtocol {
     func request<T: Decodable>(endpoint: Endpoint, decoder: JSONDecoder) async throws -> T
 }
+
+internal final class Networking: NetworkProtocol {
+    
+    private let session: URLSession
+    
+    init(session: URLSession = URLSession.shared) {
+        self.session = session
+    }
+    
+    func request<T: Decodable>(endpoint: Endpoint, decoder: JSONDecoder) async throws -> T {
+        var component = URLComponents()
+        component.scheme = endpoint.scheme
+        component.host = endpoint.baseURL
+        component.path = endpoint.path
+        component.queryItems = endpoint.parameters
+        
+        guard let url = component.url else {
+            throw NetworkError.invalidURL
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = endpoint.method
+        
+        do  {
+            let (data, response) = try await session.data(from: url)
+            guard let httpResponse = response as? HTTPURLResponse, isOK(httpResponse) else {
+                throw NetworkError.invalidData
+            }
+            return try decoder.decode(T.self, from: data)
+            
+        } catch {
+            throw NetworkError.decodableFail(error.localizedDescription)
+        }
+    }
+    
+    private func isOK(_ response: HTTPURLResponse) -> Bool {
+        (200...299).contains(response.statusCode)
+    }
+}
