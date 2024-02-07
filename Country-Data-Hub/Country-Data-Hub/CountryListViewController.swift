3dee872ff35a6e0badb32c6dc4d2d3664e162e5c
Sajal Gupta <sajalgupta4me@gmail.com>
2024-02-07 20:13:47 +0530

    created `CountryListViewController class` to render UI

--- Country-Data-Hub/Country-Data-Hub/CountryListViewController.swift
+++ Country-Data-Hub/Country-Data-Hub/CountryListViewController.swift
@@ -9,5 +9,15 @@ import UIKit
 
 final class CountryListViewController: UIViewController {
     
+    private let viewModel: CountryListViewModel
     
+    init(viewModel: CountryListViewModel) {
+        self.viewModel = viewModel
+        
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
