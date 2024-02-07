4ea055d5b25ecf7e140d920e6d0ec514cfc52643
Sajal Gupta <sajalgupta4me@gmail.com>
2024-02-08 02:07:40 +0530

    update viewModel to incorporate changes respect to the view controller

--- Country-Data-Hub/Country-Data-Hub/View/CountryListViewController.swift
+++ Country-Data-Hub/Country-Data-Hub/View/CountryListViewController.swift
@@ -22,6 +22,8 @@ final class CountryListViewController: UIViewController {
         return label
     }()
     
+    let searchViewController = UISearchController(searchResultsController: SearchResultsViewController())
+    
     private let tableView: UITableView = {
         let tableView = UITableView()
         tableView.register(CountryTableViewCell.self, forCellReuseIdentifier: String(describing: CountryTableViewCell.self))
@@ -51,16 +53,47 @@ final class CountryListViewController: UIViewController {
         title = "Country List"
         
         setupNavigationBar()
+        configureSearchBar()
         setupDataSource()
         bindUI()
         viewModel.fetchCountryList()
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        searchViewController.searchResultsUpdater = self
+        
+        navigationItem.searchController = searchViewController
+        navigationItem.hidesSearchBarWhenScrolling = false
+        
+        definesPresentationContext = true
+    }
+    
     private func setupNavigationBar() {
         navigationItem.leftBarButtonItem = UIBarButtonItem(customView: leftBarButtonLabel)
-        //imageView.image = UIImage(systemName: "line.3.horizontal.decrease.circle")
         
         navigationItem.rightBarButtonItem =  UIBarButtonItem(image: UIImage(systemName: "person.crop.circle.fill"), style: .plain, target: self, action:  #selector(rightButtonAction))
+    }
+    
+    private func configureSearchBar() {
+        searchViewController.searchBar.searchTextField.clearButtonMode = .never
+
+        let searchTextField = searchViewController.searchBar.searchTextField
+        searchTextField.attributedPlaceholder = NSAttributedString(string: "Search by Country", attributes: [.font: UIFont.systemFont(ofSize: 16, weight: .regular), .foregroundColor: UIColor.gray])
+        
+        searchTextField.font = UIFont.systemFont(ofSize: 14)
+        searchTextField.backgroundColor = UIColor(red: 248 / 255.0, green: 248 / 255.0, blue: 248 / 255.0, alpha: 1)
+        searchTextField.borderStyle = .none
+        searchTextField.layer.borderColor = UIColor.black.withAlphaComponent(0.08).cgColor
+        searchTextField.layer.borderWidth = 1.0
+        searchTextField.layer.cornerRadius = 8
+        
+        searchViewController.searchBar.setLeftImage(UIImage(systemName: "text.magnifyingglass"))
+        
+        searchViewController.searchBar.barTintColor = .clear
+        searchViewController.searchBar.setImage(UIImage(systemName: "line.3.horizontal.decrease.circle.fill"), for: .bookmark, state: .normal)
+        searchViewController.searchBar.showsBookmarkButton = true
+        searchViewController.searchBar.delegate = self
     }
 
     @objc func rightButtonAction(sender: UIBarButtonItem) {
@@ -78,10 +111,13 @@ final class CountryListViewController: UIViewController {
     }
     
     private func bindUI() {
+//        if viewModel.state == .loading {
+//            
+//        }
         viewModel.onSuccess = { [weak self] in
             guard let self = self else { return }
             
-            self.updateDataSource(countryList: self.viewModel.countryList)
+            self.updateDataSource(countryList: self.viewModel.country)
         }
         viewModel.onError = { error in
             // TODO
@@ -93,8 +129,57 @@ final class CountryListViewController: UIViewController {
         var snapshot = NSDiffableDataSourceSnapshot<Section, CountryModel>()
         
         snapshot.appendSections([.main])
-        snapshot.appendItems(countryList)
+        if case .populationFilterActive(let countryList) = viewModel.state {
+            snapshot.appendItems(countryList)
+        } else if case .coutryListLoaded(let countryList) = viewModel.state {
+            snapshot.appendItems(countryList)
+        }
         
-        dataSource?.apply(snapshot, animatingDifferences: false)
+        dataSource?.apply(snapshot, animatingDifferences: true)
+    }
+}
+
+extension CountryListViewController: UISearchResultsUpdating, UISearchBarDelegate {
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        if let searchText = searchController.searchBar.text {
+            let filteredList = viewModel.getCountryList(for: searchText)
+            
+            self.update(searchController: searchController, countryList: filteredList)
+        }
+    }
+    
+    private func update(searchController: UISearchController, countryList: [CountryModel]) {
+        if let searchController = searchController.searchResultsController as? SearchResultsViewController {
+            searchController.countryList = countryList
+        }
+
+    }
+    
+    func searchBarBookmarkButtonClicked(_ searchBar: UISearchBar) {
+        let alertController = UIAlertController(title: "Population", message: "Filter Population based on the Selcted Field", preferredStyle: .actionSheet)
+        
+        alertController.addAction(UIAlertAction(title: PopulationFilter.lessThan1Million.stringValue, style: .default, handler: { action in
+            self.filter(population: .lessThan1Million)
+        }))
+        
+        alertController.addAction(UIAlertAction(title: PopulationFilter.lessThan5Million.stringValue, style: .default, handler: { action in
+            self.filter(population: .lessThan5Million)
+        }))
+        
+        alertController.addAction(UIAlertAction(title: PopulationFilter.lessThan10Million.stringValue, style: .default, handler: { action in
+            self.filter(population: .lessThan10Million)
+        }))
+        
+        alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: { action in 
+            self.viewModel.resetPopulationFilter()
+            alertController.dismiss(animated: true)
+        }))
+        
+        self.present(alertController, animated: true)
+    }
+    
+    private func filter(population: PopulationFilter) {
+       self.viewModel.getCountryList(for: population)
     }
 }
--- Country-Data-Hub/Country-Data-Hub/ViewModel/CountryListViewModel.swift
+++ Country-Data-Hub/Country-Data-Hub/ViewModel/CountryListViewModel.swift
@@ -7,10 +7,54 @@
 
 import Foundation
 
+enum PopulationFilter {
+    case lessThan1Million
+    case lessThan5Million
+    case lessThan10Million
+    
+    var number: Int {
+        switch self {
+        case .lessThan1Million:
+            return 10_000_00
+        case .lessThan5Million:
+            return 50_000_00
+        case .lessThan10Million:
+            return 10_000_000
+        }
+    }
+    
+    var stringValue: String {
+        switch self {
+        case .lessThan1Million:
+            return "< 1 M"
+        case .lessThan5Million:
+            return "< 5 M"
+        case .lessThan10Million:
+            return "< 10 M"
+        }
+    }
+}
+
 internal final class CountryListViewModel {
     
     private let usecase: UsecaseProtocol
-    private(set) var countryList = [CountryModel]()
+    private var countryList = [CountryModel]()
+    
+    enum State {
+        case loading
+        case populationFilterActive([CountryModel])
+        case coutryListLoaded([CountryModel])
+    }
+    
+    private(set) var state: State = .loading {
+        didSet {
+            onSuccess?()
+        }
+    }
+    
+    var country:  [CountryModel] {
+        countryList
+    }
     
     internal var onError: ((String) -> Void)?
     internal var onSuccess: (() -> Void)?
@@ -23,13 +67,36 @@ internal final class CountryListViewModel {
     func fetchCountryList() {
         Task {
             do {
+                
                 let countryListModel = try await usecase.fetchCountryList()
-                countryList.append(contentsOf: countryListModel)
-                onSuccess?()
+               
+                let sortedList = countryListModel.sorted {
+                    return ($0.name.localizedCaseInsensitiveCompare($1.name) == .orderedAscending)
+                }
+                
+                countryList.append(contentsOf: sortedList)
+                state = .coutryListLoaded(sortedList)
             } catch {
-                print(error)
                 onError?(error.localizedDescription)
             }
         }
+    }
+    
+    func getCountryList(for text: String) -> [CountryModel] {
+        return countryList.filter { model in
+            model.name.hasPrefix(text)
+        }
+    }
+    
+    func getCountryList(for population: PopulationFilter){
+        let filteredList = countryList.filter { model in
+            model.population ?? 0 < population.number
+        }
+        
+        state = .populationFilterActive(filteredList)
+    }
+    
+    func resetPopulationFilter() {
+        self.state = .coutryListLoaded(countryList)
     }
 }
