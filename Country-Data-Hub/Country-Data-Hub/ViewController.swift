f18c3f157d3dee8aaa5c72ef84fa103a79ba2aab
Sajal Gupta <sajalgupta4me@gmail.com>
2024-02-07 18:40:44 +0530

    create new Country-Data-Hub project with xcode 15.0.1 and swift language

---
+++ Country-Data-Hub/Country-Data-Hub/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Country-Data-Hub
+//
+//  Created by Sajal Gupta on 07/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
